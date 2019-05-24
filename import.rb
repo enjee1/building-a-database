bc02b159db5a9eb43cacdfc8fa82ba419bf2209a
enjee <ngiarrusso@gmail.com>
2019-05-23 21:06:27 -0400

    Update import.rb to add records to all tables

--- import.rb
+++ import.rb
@@ -1,5 +1,6 @@
 require "pg"
 require "csv"
+require "pry"
 
 def db_connection
   begin
@@ -14,18 +15,50 @@ csv_records = CSV.readlines("data.csv", headers: true)
 
 zoning_types = csv_records.map { |record| record["zoning_type"]}.uniq
 zoning_types.each do |type|
-  sql = "INSERT INTO zoning_types (name) VALUES ($1)"
   db_connection do |conn|
-    require "pry"
-    binding.pry
-    conn.exec_params(sql, [type])
+    result = conn.exec_params(
+      "SELECT name FROM zoning_types WHERE name=$1", [type]
+    )
+
+    if result.to_a.empty?
+      sql = "INSERT INTO zoning_types (name) VALUES ($1)"
+      conn.exec_params(sql, [type])
+    end
   end
 end
 
 construction_types = csv_records.map { |record| record["construction_type"]}.uniq
 construction_types.each do |type|
-  sql = "INSERT INTO construction_types (name) VALUES ($1)"
   db_connection do |conn|
-    conn.exec_params(sql, [type])
+    result = conn.exec_params(
+      "SELECT name FROM construction_types WHERE name=$1", [type]
+    )
+
+    if result.to_a.empty?
+      sql = "INSERT INTO construction_types (name) VALUES ($1)"
+      conn.exec_params(sql, [type])
+    end
+  end
+end
+
+db_connection do |conn|
+  csv_records.each do |record|
+    name = record["name"]
+    construction_type = record["construction_type"]
+    zoning_type = record["zoning_type"]
+
+    construction_type_id = conn.exec_params(
+      "SELECT id FROM construction_types WHERE name=$1",
+      [construction_type]
+    )[0]["id"]
+
+    zoning_type_id = conn.exec_params(
+      "SELECT id FROM zoning_types WHERE name=$1",
+      [zoning_type]
+    )[0]["id"]
+
+    conn.exec_params("INSERT INTO accounts (name, construction_type_id, zoning_type_id)
+      VALUES ($1, $2, $3)", [name, construction_type_id, zoning_type_id]
+    )
   end
 end
